85d7287b2d854ede82400008fd4963c7529a5193
Paul Tondeur <paul@paultondeur.nl>
2013-10-06 22:39:43 +0200

    recursive toJSON implemented and tests added

---
+++ spec/javascripts/toJSONSpec.js
@@ -0,0 +1,53 @@
+describe("toJSON", function() {
+
+	it("recursively calls the toJSON method on directly nested models", function() {
+		var modelA = new Backbone.Model({name:'modelA'});
+		var modelB = new Backbone.Model({name:'modelB'});
+		modelA.set({otherModel:modelB});
+
+		var jsonData = modelA.toJSON();
+
+		expect(jsonData.name).toEqual('modelA');
+		expect(jsonData.otherModel.name).toEqual('modelB');
+	});
+
+	it("recursively calls the toJSON method on nested models in an array", function() {
+		var modelA = new Backbone.Model({name:'modelA'});
+		var modelB = new Backbone.Model({name:'modelB'});
+		var modelC = new Backbone.Model({name:'modelC'});
+		modelA.set({models:[modelB]});
+		modelB.set({models:[modelC]});
+
+		var jsonData = modelA.toJSON();
+
+		expect(jsonData.models[0].name).toEqual('modelB');
+		expect(jsonData.models[0].models[0].name).toEqual('modelC');
+	});
+
+	it("recursively calls the toJSON method on nested models in a collection", function() {
+		var modelA = new Backbone.Model({name:'modelA'});
+		var modelB = new Backbone.Model({name:'modelB'});
+		var modelC = new Backbone.Model({name:'modelC'});
+		modelA.set({models:new Backbone.Collection([modelB])});
+		modelB.set({models:new Backbone.Collection([modelC])});
+
+		var jsonData = modelA.toJSON();
+
+		expect(jsonData.models[0].name).toEqual('modelB');
+		expect(jsonData.models[0].models[0].name).toEqual('modelC');
+	});
+
+	it("Doesn't work with circular relationships for now", function() {
+		var modelA = new Backbone.Model({name:'modelA'});
+		var modelB = new Backbone.Model({name:'modelB'});
+		modelA.set({other:[modelB]});
+		modelB.set({other:[modelA]});
+
+		expect(function() {var jsonData = modelA.toJSON()}).toThrow(new Error("Maximum call stack size exceeded"));
+
+//		expect(jsonData.name).toEqual('modelA');
+//		expect(jsonData.other.name).toEqual('modelB');
+//		expect(jsonData.other.other.name).toEqual('modelA');
+	});
+
+});
